81b2f05b6d62d758bc2ed2ba42874fab149fc44a
DevMaxoDev <124282256+DevMaxoDev@users.noreply.github.com>
2025-10-03 13:46:08 +0000

    feat: Exercices sur les fondamentaux JS terminés

---
+++ tp-js-bases/script.js
@@ -0,0 +1,33 @@
+const prenom = "Maxandre";
+const nom = "PLESSIS";
+//nom = PARLA; // Cette valeur n'est pas pris en compte car il ne peut que exister une seul conctente avec le meme nom "nom"
+//let age = 17;
+//age = 18; // Cette valeur prend le dessus sur la valeur de la ligne 3
+
+let age = 17;
+
+if (age < 13) {
+    console.log("Accès interdit aux moins de 13 ans.");
+} else if (age < 18) {
+    console.log("Vous pouvez accéder à la section ado.");
+} else {
+    console.log("Bienvenue dans l'espace adulte.");
+}
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`7 x ${i} = ${7 * i}`);
+}
+
+
+const saluer = (prenom) => `Bonjour, ${prenom} ! Comment vas-tu ?`;
+
+const message = saluer("Maxandre");
+console.log(message);
+
+
+
+
+console.log(nom);
+console.log(prenom);
+console.log(age);
